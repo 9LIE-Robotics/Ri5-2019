18508488e3886560e2095afb7462de1f03f19ca2
Jonathan Heidegger <jth3141@gmail.com>
2020-01-07 17:29:17 -0500

    basics added for trajectory control

--- infiniteReee/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ infiniteReee/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,10 +7,19 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import frc.robot.OI;
 
 public class Drivetrain extends SubsystemBase {
@@ -18,7 +27,11 @@ public class Drivetrain extends SubsystemBase {
   private final CANSparkMax m_rightMotor;
   private final CANSparkMax m_leftMotor;
   private final DifferentialDrive dfDrive;
-  
+  private DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(28));
+  private DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0));
+  private AHRS gyro; 
+  private Pose2d pose = new Pose2d();
+  private SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(1, 1, 1);
   public enum systemStates {
     NEUTRAL,
     OPEN_LOOP
@@ -35,16 +48,42 @@ public class Drivetrain extends SubsystemBase {
     m_leftMotor = new CANSparkMax(2, MotorType.kBrushless);
     dfDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
     currentState = systemStates.OPEN_LOOP;
+    gyro = new AHRS(SPI.Port.kMXP);
+    m_rightMotor.getEncoder().setPositionConversionFactor(1.0 / 10.75 * Math.PI * Units.inchesToMeters(4));
+    m_leftMotor.getEncoder().setPositionConversionFactor(1.0 / 10.75 * Math.PI * Units.inchesToMeters(4));
+    resetEncoders();
   }
   
   
   /**
-   * @return the instance of the drive train to be used
+   * @return the instance of the drive tra
+   * in to be used
    */
   public static Drivetrain getInstance() {
     return instance;
   }
-
+  public Rotation2d getHeading() {
+    return  Rotation2d.fromDegrees(-gyro.getAngle());
+  }
+  public void resetGyro() {
+    gyro.reset();
+  }
+  public DifferentialDriveWheelSpeeds getSpeeds() {
+    return new DifferentialDriveWheelSpeeds(
+      m_leftMotor.getEncoder().getVelocity() / 10.75 * Units.inchesToMeters(4) * Math.PI / 60.0,
+      m_rightMotor.getEncoder().getVelocity() / 10.75 * Units.inchesToMeters(4) * Math.PI / 60.0  
+    );
+  }
+  public double getRightDistance() {
+    return m_rightMotor.getEncoder().getPosition();
+  }
+  public double getLeftDistance() {
+    return m_leftMotor.getEncoder().getPosition();
+  }
+  public void resetEncoders() {
+    m_leftMotor.getEncoder().setPosition(0);
+    m_rightMotor.getEncoder().setPosition(0);
+  }
   private void defaultStateChange() {
 		if(requestedState!=currentState) {
 			currentState = requestedState;
@@ -58,6 +97,16 @@ public class Drivetrain extends SubsystemBase {
   private void arcade() {
     dfDrive.arcadeDrive(OI.getDriveAmount(), OI.getSteerAmount());
   }
+  public void tankVoltageDrive(double leftVoltage, double rightVoltage) {
+    m_leftMotor.setVoltage(leftVoltage);
+    m_rightMotor.setVoltage(rightVoltage);
+  }
+
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+
 
   @Override
   public void periodic() {
@@ -71,6 +120,7 @@ public class Drivetrain extends SubsystemBase {
     //     tank();
     //     break;
     // }
+    pose = odometry.update(getHeading(), getLeftDistance(), getRightDistance());
     arcade();
     defaultStateChange();
   }
